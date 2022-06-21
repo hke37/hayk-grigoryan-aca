16f4793c3e93192637ea575af9133f4a484fb3ce
hke37 <hayko-g84@mail.ru>
2022-06-21 12:56:38 +0400

    player class functions were added

--- src/Music.py
+++ src/Music.py
@@ -32,6 +32,11 @@ class Playlist:
     def __init__(self, s):
         self.__songs = s
 
+    @property
+    def songs(self):
+        return self.__songs
+
+
     def load_songs(self):
         with open('albums.txt') as f:
             for i in f.readlines():
@@ -57,8 +62,9 @@ class Player:
     def __init__(self, pl):
         self.__is_valid = False
         self.__playlist = pl
+        self.__current_song = self.__playlist.songs[0]
+        self.__status = False
         self.__is_valid = True
-
 
     @property
     def playlist(self):
@@ -72,38 +78,46 @@ class Player:
         return "{}".format(self.__playlist)
 
 
-    def play(self,a):
-        for x in self.__playlist.name:
-            print(a)
+    def get_current_song(self):
+        if self.is_valid:
+            return self.__current_song
 
+    def play(self):
+        self.__status = True
+        return self.__current_song
 
+    def stop(self):
+        self.__status = False
+        return self.__current_song
 
-    # def stop(self, a):
-    #     pass
-    #
-    # def previous_song(self,a):
-    #     pass
-    #
-    # def show_current_song(self):
-    #     pass
+    def select_song(self, x):
+        if self.is_valid:
+            self.__current_song = self.__playlist.songs[x]
+        return self.__current_song
+
+    def previous_song(self):
+        if self.is_valid:
+            a = self.__playlist.songs.index(self.__current_song) - 1
+            return self.__playlist.songs[a]
+
 
 
 ################################################
 
-song1 = Song('Eric Clapton', "Journeyman", '1969', 'Old Love')
-# print(song1)
-song2 = Song('Eric Clapton', "JJJ", '1990', 'OOO')
-song3 = Song('Eric Clapton', "KKK", '2000', '111')
+# song1 = Song('Eric Clapton', "Journeyman", '1969', 'Old Love')
 # plist1 = Playlist([song1,song2,song3])
-# plist2 = Playlist([song2,song3])
 # print(plist1)
 plist3 = Playlist([])
 plist3.load_songs()
-# plist1.load_songs()
-# print(plist3)
-
 pl1 = Player(plist3)
 # print(pl1)
-pl1.play(4)
-# print(pl1)
-
+a = pl1.get_current_song()
+b = pl1.play()
+c = pl1.stop()
+print(a)
+print(b)
+print(c)
+d = pl1.select_song(10)
+print(d)
+e = pl1.previous_song()
+print(e)
